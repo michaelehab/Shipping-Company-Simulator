417a1d7645fa939bbc23069f125b0b0f55c22cfe
Ebraam atef <ebraamatef@outlook.com>
2022-04-01 17:29:06 +0200

    done by EA  queue data structure

--- ShippingCompany/Queue.h
+++ ShippingCompany/Queue.h
@@ -1,1 +1,108 @@
 #pragma once
+// Done by EA
+#include<iostream>
+#include"Node.h"
+using namespace std;
+
+template <class T>
+class LinkedQueue {
+	
+	Node* Fptr;
+	Node* Bptr;
+
+public:
+	LinkedQueue()
+	{
+		Fptr = nullptr;
+		Bptr = nullptr;
+	}
+	bool IsEmpty()  const
+	{
+		if (Fptr == nullptr && Bptr == nullptr)
+			return true;
+		else return false;
+	}
+	bool enQueue(const T& nData);
+	bool deQueue(T& Fdata);
+	bool peek(T& Fdata) const;
+	LinkedQueue(const LinkedQueue<T>& LQ);
+	~LinkedQueue();
+
+};
+template<class T>
+bool LinkedQueue<T>:: enQueue(const T& nData)
+{
+	Node<T>* newNode = new Node<T>;
+	if (IsEmpty())
+	{
+		Fptr = newNode;
+		Bptr = newNode;
+	}
+	else
+	{
+		Bptr->setNext(newNode);
+		newNode->setNext(nullptr);
+		Bptr = newNode;
+	}
+	return true;
+}
+template<class T>
+bool LinkedQueue<T>::deQueue(T& Fdata)
+{
+	Node<T>* currPtr;
+	if (IsEmpty())
+	{
+		cout << "Queue is empty" << endl;
+		return false;
+	}
+	else
+	{
+		currPtr = Fptr;
+		Fdata = currPtr->getItem();
+		Fptr = Fptr->getNext();
+		delete currPtr;
+	}
+	return true;
+}
+
+template <typename T>
+bool LinkedQueue<T>::peek(T& Fdata) const
+{
+	if (isEmpty())
+		return false;
+
+	frntEntry = frontPtr->getItem();
+	return true;
+
+}
+template <class T>
+LinkedQueue<T>::~LinkedQueue()
+{
+	T temp;
+	while (deQueue(temp));
+}
+
+template <typename T>
+LinkedQueue<T>::LinkedQueue(const LinkedQueue<T>& LQ)
+{
+	Node<T>* NodePtr = LQ.frontPtr;
+	if (!NodePtr) //LQ is empty
+	{
+		frontPtr = backPtr = nullptr;
+		return;
+	}
+
+	//insert the first node
+	Node<T>* ptr = new Node<T>(NodePtr->getItem());
+	Fptr = Bptr = ptr;
+	NodePtr = NodePtr->getNext();
+
+	//insert remaining nodes
+	while (NodePtr)
+	{
+		Node<T>* ptr = new Node<T>(NodePtr->getItem());
+		backPtr->setNext(ptr);
+		backPtr = ptr;
+		NodePtr = NodePtr->getNext();
+	}
+}
