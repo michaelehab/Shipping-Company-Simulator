6ca37f932a013a3671b10fda19bff0d820a576d8
Ebraam atef <ebraamatef@outlook.com>
2022-04-02 11:51:57 +0200

    queue done by EA for(implemention data structure)

--- ShippingCompany/Cargo.cpp
+++ ShippingCompany/Cargo.cpp
@@ -1,1 +1,42 @@
 #include "Cargo.h"
+#include<iostream>
+using namespace std;
+Time::Time(int h=0, int d=0)
+{
+	set_time( h, d);
+}
+
+void Time::set_H(int h)
+{
+	if (h > 0)
+		hour = h;
+	else cout << "Please enter positive number" << endl;
+}
+
+void Time::set_D(int d)
+{
+	if (d > 0)
+		day = d;
+	else cout << "Please enter positive number" << endl;
+}
+
+void Time::set_time(int h, int d)
+{
+	set_D(d);
+	set_H(h);
+}
+
+int Time::get_d()
+{
+	return day;
+}
+
+int Time::get_h()
+{
+	return hour;
+}
+
+void Time::print_time()
+{
+	cout << "the Time is ( " << get_d() << " : " << get_h << " )" << endl;
+}
